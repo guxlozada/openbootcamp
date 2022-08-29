a5dc63c0b0b003e05644950b8d7f4da144589292
Gustavo Lozada <glozada@gestor.co>
2022-08-29 12:49:34 -0500

    3JB: Completar ejercicios tema 1

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# openbootcamp
+openbootcamp
+=============================
 
 Repositorio temporal para openbootcamp
 
-## Cursos
+# Cursos
 
-- [Introducción a la programación](#introduccion-programacion).
+- [Introducción a la programación](#introducción-a-la-programación)
+- [Java básico](#java-básico)
 
-### Introducción a la programación
+## Introducción a la programación
 
 - [x] Ejercicio 3
 - [x] Ejercicio 4
 - [x] Ejercicio 8
 - [x] Ejercicio 9
+
+## Java básico
+
+- [x] Ejercicio 1
+
+
+
+
---
+++ java-basico/src/main/java/org/example/EjerciciosTema1.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class EjerciciosTema1 {
+
+    public static void main(String[] args) {
+        // Numericos
+        byte variable1=5;
+        System.out.printf("byte:%d%n", variable1);
+        short variable2=10;
+        System.out.printf("short:%d%n", variable2);
+        int variable3=15;
+        System.out.printf("int:%d%n", variable3);
+        long variable4=20;
+        System.out.printf("long:%d%n", variable4);
+
+        // Decimales
+        float variable5= 1.1f;
+        System.out.printf("float:%f%n", variable5);
+        double variable6= 2.2d;
+        System.out.printf("double:%f%n", variable6);
+
+        // Booleano
+        boolean variable7 =true;
+        System.out.printf("boolean:%b%n", variable7);
+        boolean variable8 =false;
+        System.out.printf("boolean2:%b%n", variable8);
+
+        //Texto
+        char variable9 ='a';
+        System.out.printf("char:%s%n", variable9);
+        String variable10 ="texto";
+        System.out.printf("String:%s", variable10);
+    }
+}
